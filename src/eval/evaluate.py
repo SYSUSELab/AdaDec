a4d503f51ecfd6b11f6ab0f7561658788657c3d1
Arw-Wow <hekaifeng70@gmail.com>
2025-09-04 15:06:38 +0800

    update

--- src/eval/evaluate.py
+++ src/eval/evaluate.py
@@ -439,7 +439,7 @@ def load_deveval_dataset(file_path):
                 "namespace": data["namespace"],
                 "prompt": data["prompt"]
             })
-    return problems[100:]
+    return problems[100:] # 取100之后的数据
 
 
 def extract_last_function_body(code_str: str) -> str:
